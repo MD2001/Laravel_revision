54f090a2a57f56d7a870cd2a0ff9d451c5d153cb
Mohamed Diaa <mdiea0581@gmail.com>
2024-10-21 13:57:34 +0300

    clean code (route)

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,17 @@
 
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
-
 
 Route::get('/', function () {
     return view('Home');
 });
 
-//Job::index
 Route::get('/jobs',[JobController::class,'index']);
-//Job:create.show
 Route::get('/jobs/create',[JobController::class,"ShowCreate"]);
-//Job::show
 Route::get('/jobs/{job}',[JobController::class,"show"]);
-//Job::create
 Route::post('/jobs',[JobController::class,'create']);
-//Job::edite
 Route::get('/jobs/{job}/edit',[JobController::class,'edite']);
-//Job::Update
 Route::patch('/jobs/{job}',[JobController::class,'update']);
-//Job::Delete
 Route::delete('/jobs/{job}',[JobController::class,'delete']);
 
 Route::get('/contact', function () {
